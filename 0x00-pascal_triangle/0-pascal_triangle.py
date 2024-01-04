523309bfac40f5be5001463fa7a61dac8adbf81e
samie54 <126026530+samie54@users.noreply.github.com>
2024-01-04 11:42:29 -0900

    Task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,13 +11,13 @@ def pascal_triangle(n):
     if n <= 0:
         return triangle
     for i in range(n):
-        temp = []
+        blub = []
 
         for j in range(i+1):
             if j == 0 or j == i:
-                temp.append(1)
+                blub.append(1)
             else:
-                temp.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(temp)
+                blub.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(blub)
 
     return triangle
