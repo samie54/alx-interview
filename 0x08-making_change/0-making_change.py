f07805a8a92f3e646fdc49c6ea1b98678d3d0b4f
samie54 <126026530+samie54@users.noreply.github.com>
2024-02-28 07:49:38 -0800

    MKChangeTask0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""Making module changement
+
+"""
+Making change
 """
 
 
 def makeChange(coins, total):
-    """Determines fewest number of coins; needed to meet given
-    total amount when a given pile of coins of different values
-    """
     if total <= 0:
         return 0
-    rem = total
-    coins_count = 0
-    coin_idx = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
-        else:
-            coin_idx += 1
-    return coins_count
 
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
